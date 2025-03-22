a915f3d0c9c9f05fce5ff4a804aa5421dcdf9409
Eniola Hannah <heniolahannah@gmail.com>
2025-03-22 03:36:47 +0100

    Checks for duplicate

--- Query.sql
+++ Query.sql
@@ -25,3 +25,28 @@ UPDATE pixar_films
 SET Release_date = STR_TO_DATE(Release_date, "%m/%d/%YYYY");
 ALTER TABLE pixar_films
 MODIFY COLUMN Release_date DATE;
+
+-- checking for duplicates
+SELECT film, award_type, status, COUNT(*) AS count FROM academy
+GROUP BY film, award_type, status
+HAVING COUNT(*) > 1;
+
+SELECT film, budget, box_office_us_canada, box_office_other, box_office_worldwide, COUNT(*) AS count FROM box_office
+GROUP BY film, budget, box_office_us_canada, box_office_other, box_office_worldwide
+HAVING COUNT(*) > 1;
+
+SELECT film, category, value, COUNT(*) AS count FROM genres
+GROUP BY film, category, value
+HAVING COUNT(*) > 1;
+
+SELECT Number, film, Release_date, Run_time, Film_rating, COUNT(*) AS count FROM pixar_films
+GROUP BY Number, film, Release_date, Run_time, Film_rating
+HAVING COUNT(*) > 1;
+
+SELECT film, rotten_tomatoes_score, rotten_tomatoes_counts, metacritic_score, metacritic_counts, COUNT(*) AS count FROM public_response
+GROUP BY film, rotten_tomatoes_score, rotten_tomatoes_counts, metacritic_score, metacritic_counts
+HAVING COUNT(*) > 1;
+
+SELECT film, role_type, name, COUNT(*) AS count FROM pixar_people    -- Duplicate values present!
+GROUP BY film, role_type, name
+HAVING COUNT(*) > 1;
